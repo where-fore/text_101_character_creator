a07f90af25aeb8a94cdeedd5d90ffddebd86bf1a
Dover <patrick_dover@yahoo.ca>
2019-07-23 16:32:53 -0400

    tweaked lighting, started to script
    changed ambient lighting

---
+++ Assets/Scripts/Slide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Slide : MonoBehaviour
+{
+    private string bodyText = "";
+    public void SetBodyText(string newBodyText) {bodyText = newBodyText;}
+
+    private TextMeshProUGUI myTextComponent= null;
+
+    private void Start()
+    {
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        myTextComponent.text = bodyText;
+    }
+}
---
+++ Assets/Scripts/Slides/StrangeAnimal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrangeAnimal : Slide
+{
+    private void FirstOption()
+    {
+        // str +1
+    }
+    private void SecondOption()
+    {
+        // dex +1
+    }
+    private void ThirdOption()
+    {
+        // int +1
+    }
+
+}
