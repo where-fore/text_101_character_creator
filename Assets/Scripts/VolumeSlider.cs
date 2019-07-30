71de9fd127c22a453e26b925ea431daffb01410e
Dover <patrick_dover@yahoo.ca>
2019-07-29 21:22:33 -0400

    brief fiddles

---
+++ Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    private List<AudioSource> audioSources = new List<AudioSource>();
+    [SerializeField] private GameObject audioSourceManager = null;
+    private Slider mySlider = null; 
+    void Start()
+    {
+        mySlider = GetComponent<Slider>();
+
+        foreach (AudioSource source in audioSourceManager.GetComponentsInChildren<AudioSource>())
+        {
+            audioSources.Add(source);
+        }
+    }
+
+    void Update()
+    {
+        float sliderValue = mySlider.value;
+        foreach (AudioSource source in audioSources)
+        {
+            source.volume = sliderValue;
+        }
+    }
+}
