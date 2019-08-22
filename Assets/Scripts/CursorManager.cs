29c6a3142b9032f077b2d6566245bfbf32bda0d0
Dover <patrick_dover@yahoo.ca>
2019-08-22 17:59:22 -0400

    added custom cursor

---
+++ Assets/Scripts/CursorManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    [Tooltip("Place within me a Prefab of the cursor you would like.")]
+    [SerializeField] private GameObject customCursorPrefab = null;
+    private GameObject customCursorObject = null;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        CreateCursorObject();
+    }
+
+    void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        customCursorObject.transform.position = cursorPos;
+    }
+
+    private void CreateCursorObject()
+    {
+        customCursorObject = Instantiate(customCursorPrefab);
+        customCursorObject.transform.parent = this.transform;
+    }
+}
