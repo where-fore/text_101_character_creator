0708595c96ba64ba8dc4be552fffcca0a2df096c
Dover <patrick_dover@yahoo.ca>
2019-08-22 17:10:38 -0400

    menu animation progressed!
    finished for now

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject escapeMenuParent = null;
+    private Animator escapeMenuParentAnimator = null;
+    private string escapeMenuExitAnimationTriggerStringReference = "Exit Menu";
 
     private void Start()
     {
         escapeMenuParent.SetActive(false);
+        escapeMenuParentAnimator = escapeMenuParent.GetComponent<Animator>();
     }
 
     private void Update()
     {
         if (Input.GetKeyUp(KeyCode.Escape))
         {
-            escapeMenuParent.SetActive(!escapeMenuParent.activeSelf);
+            if (escapeMenuParent.activeSelf == false)
+            {
+                OpenEscapeMenu();
+            }
+            else if (escapeMenuParent.activeSelf == true)
+            {
+                CloseEscapeMenu();
+            }
         }
+    }
+
+    private void OpenEscapeMenu()
+    {
+        escapeMenuParent.SetActive(true);
+    }
+
+    private void CloseEscapeMenu()
+    {
+        escapeMenuParentAnimator.SetTrigger(escapeMenuExitAnimationTriggerStringReference);
     }
 
     public void ExitGame()
     {
         Application.Quit();
-        Debug.Log("Exited Game");
+        Debug.Log("Application.Quit() function called.");
     }
 }
