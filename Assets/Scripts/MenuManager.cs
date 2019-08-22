96269e44539b7b6fee232f63ab81c34ba3705264
Dover <patrick_dover@yahoo.ca>
2019-08-22 15:43:25 -0400

    escape menu!
    with basic animation

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject escapeMenuParent = null;
+
+    private void Start()
+    {
+        escapeMenuParent.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            escapeMenuParent.SetActive(!escapeMenuParent.activeSelf);
+        }
+    }
+
     public void ExitGame()
     {
         Application.Quit();
